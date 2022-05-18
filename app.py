198900396d289a26703ac07f4549e3e2ac0336ec
KRISHNA <67220291+KrishnaSingh26@users.noreply.github.com>
2022-05-18 20:22:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import pickle
+import string
+import time
+from nltk.corpus import stopwords
+import nltk
+nltk.download('punkt') 
+nltk.download('stopwords') 
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+import streamlit as st
+
+with st.sidebar:
+    st.title("Welcome To Project")
+   
+
+    with st.spinner("Model Loading..."):
+        time.sleep(5)
+    st.success("Model Compiled Successfully!")
+
+st.title("Email/SMS Spam Message Detection Using ML")
+
+input_sms = st.text_area("Enter the message:")
+
+if st.button('Predict'):
+
+    transformed_sms = transform_text(input_sms)
+
+    vector_input = tfidf.transform([transformed_sms])
+
+    result = model.predict(vector_input)[0]
+
+    if result == 1:
+        st.header("Your Message is Spam")
+    else:
+        st.header("Your Message is Not Spam")
+
+
